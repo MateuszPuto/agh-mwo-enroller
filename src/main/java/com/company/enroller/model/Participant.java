4a970ed55f938edc5f55fa24aa7de3aa058a7443
MateuszPuto <mateuszmputo@gmail.com>
2022-09-23 16:48:18 +0200

    get by id

--- src/main/java/com/company/enroller/model/Participant.java
+++ src/main/java/com/company/enroller/model/Participant.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import java.io.Serializable;
+
 @Entity
 @Table(name = "participant")
 public class Participant {
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,10 +2,14 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.PersistentObjectException;
+import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
+
+import javax.servlet.http.HttpSession;
 
 @Component("participantService")
 public class ParticipantService {
@@ -16,10 +20,33 @@ public class ParticipantService {
 		connector = DatabaseConnector.getInstance();
 	}
 
+	public Participant findByLogin(String login) {
+		Session session = connector.getSession();
+		Participant participant = session.get(Participant.class, login);
+		session.close();
+
+		return participant;
+	}
+
 	public Collection<Participant> getAll() {
 		String hql = "FROM Participant";
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
+	}
+
+	public boolean addParticipant(Participant participant) {
+		Session session = connector.getSession();
+
+		try {
+			session.persist(participant);
+
+		} catch (PersistentObjectException e) {
+			return false;
+		}
+
+		session.close();
+
+		return true;
 	}
 
 }
